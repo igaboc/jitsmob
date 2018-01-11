76bb212bcfd74be2d2002b87b863361eda4589a5
Daniel Huong <daniel.huong86@gmail.com>
2018-01-11 12:49:38 +1100

    added CUD functionality to content to form Content CRUD

--- api/routes/content.js
+++ api/routes/content.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const Content = require('../models/Content')
+const authMiddleware = require('../middleware/auth')
 
 const router = new express.Router()
 
+// Read
 router.get('/contents', (req, res) => {
   Content.find()
   .then((contents) => {
@@ -11,6 +13,48 @@ router.get('/contents', (req, res) => {
   .catch((error)=> {
     res.status(400).json({error: error.message}) //if you return the whole error you may be giving away too much information
   })
+})
+
+// Create
+router.post('/contents', authMiddleware.requireJWT, (req, res) => {
+  Content.create(req.body)
+    .then((content) => {
+      res.status(201).json(content)
+    })
+    .catch((error) => {
+      res.status(400).json({ error })
+    })
+})
+
+// Update
+router.put('/contents/:id', authMiddleware.requireJWT, (req, res) => {
+  const { id } = req.params
+  Content.findByIdAndUpdate(id, req.body, { new: true, runValidators: true })
+    .then((content) => {
+      if (content) {
+        res.json(content)
+      }
+      else {
+        res.status(404).json({
+          error: new Error(`Content with id '${id}' not found`)
+        })
+      }
+    })
+    .catch((error) => {
+      res.status(400).json({ error })
+    })
+})
+
+// Remove product from wishlist
+router.delete('/contents/:id', authMiddleware.requireJWT, (req, res) => {
+  const { id } = req.params
+  const removedContent = Content.destroy(id)
+  if (removedContent) {
+    res.json(removedContent)
+  }
+  else {
+    res.status(404).json({ error: `Content with id '${id}' was not found` })
+  }
 })
 
 module.exports = router
