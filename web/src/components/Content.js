bd737510b5347a42c298884d75c8dc86fe912399
Clayton <c_w_serious-github@yahoo.com.au>
2018-01-15 10:51:08 +1100

    Basic video functionality working

--- web/src/components/Content.js
+++ web/src/components/Content.js
@@ -7,6 +7,18 @@ function Content({
   youtubeURL,
   description
 }) {
+
+  var video= "5hqYwZgNbDs"// TSKSR Budokan 2009 demo //"mYFaghHyMKc" // music video: Family of the Year - Hero [Boyhood] (Official)
+  var autoplay="0"
+  var rel="0"
+  var modest="1"
+
+  var videoSrc = "https://www.youtube.com/embed/" + 
+      video + "?autoplay=" + 
+      autoplay + "&rel=" + 
+      rel + "&modestbranding=" +
+      modest
+
   return (
     <div>
       <h2>{ title }</h2>
@@ -14,15 +26,15 @@ function Content({
       <div>Date added placeholder</div>
       <div>{ youtubeURL }</div>
       
-      
-    
-    
-
-      
-
+      <video src="tsksr_demo_budokan2009.webm"/>
       
       <h2>{ subtitle }</h2>
       <div>{ description }</div>
+
+      <div className="container">
+        <iframe className="player" type="text/html" width="100%" height="100%" src={videoSrc} frameborder="0"/>
+      </div>
+
     </div>
   )
 }
