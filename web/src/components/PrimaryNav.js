e031a648d933ea7a7cbb534b8e0465329b8e339e
Daniel Huong <daniel.huong86@gmail.com>
2018-01-16 15:00:44 +1100

    removed logos and refactored html/css

--- web/src/components/PrimaryNav.js
+++ web/src/components/PrimaryNav.js
@@ -6,7 +6,8 @@ import './PrimaryNav.css';
 
 function PrimaryNav({
   menuClassWidth,
-  onMenuClick
+  onMenuClick,
+
 }) {
   return (
   <Fragment>
@@ -16,10 +17,10 @@ function PrimaryNav({
         <img className="logo" src={ jitsmobLogo } />
       </div>
       <div className="col-10 col-lg-9 pr-5">
-        <a className="ml-4 pt-4 pb-2" href="#news">News</a>
-        <a className="mx-4 pt-4 pb-2" href="#contact">Contact</a>
-        <a className="mx-4 pt-4 pb-2" href="#about">About</a>
-        <a className="mr-4 pt-4 pb-2 active" href="#home">Home</a>
+        <a className="ml-4 pt-3 pb-2" href="#news">News</a>
+        <a className="mx-4 pt-3 pb-2" href="#contact">Contact</a>
+        <a className="mx-4 pt-3 pb-2" href="#about">About</a>
+        <a className="mr-4 pt-3 pb-2 active" href="#home">Home</a>
         <a className="py-2 icon" href="javascript:void(0);" onClick={ (event) => { onMenuClick() } }>&#9776;</a>
       </div>
       <div className="col-lg-1"/>
