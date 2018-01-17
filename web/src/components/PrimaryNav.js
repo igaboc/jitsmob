353a6513db1cd9a1b04c9c8fa1bc9e5959002a83
Daniel Huong <daniel.huong86@gmail.com>
2018-01-17 12:38:04 +1100

    added routes and routes to navbar

--- web/src/components/PrimaryNav.js
+++ web/src/components/PrimaryNav.js
@@ -17,10 +17,9 @@ function PrimaryNav({
         <img className="logo" src={ jitsmobLogo } />
       </div>
       <div className="col-10 col-lg-9 pr-5">
-        <a className="ml-4 pt-3 pb-2" href="#news">News</a>
-        <a className="mx-4 pt-3 pb-2" href="#contact">Contact</a>
-        <a className="mx-4 pt-3 pb-2" href="#about">About</a>
-        <a className="mr-4 pt-3 pb-2 active" href="#home">Home</a>
+        <a className="ml-4 pt-3 pb-2" href="/excercises">Excercises</a>
+        <a className="mx-4 pt-3 pb-2" href="/admin">Admin (for now only)</a>
+        <a className="mx-4 pt-3 pb-2" href="/login">Login (for now only)</a>
         <a className="py-2 icon" href="javascript:void(0);" onClick={ (event) => { onMenuClick() } }>&#9776;</a>
       </div>
       <div className="col-lg-1"/>
@@ -30,9 +29,9 @@ function PrimaryNav({
     <div className={`overlay ${ menuClassWidth }`}>
       <a href="javascript:void(0)" class="closebtn" onClick={ (event) => { onMenuClick() } } >&times;</a>
       <div className="overlay-content">
-        <a href="#">About</a>
-        <a href="#">Services</a>
-        <a href="#">Clients</a>
+        <a href="/excercises">About</a>
+        <a href="/admin">Admin (for now only)</a>
+        <a href="/login">Login (for now only)</a>
         <a href="#">Contact</a>
       </div>
     </div>
