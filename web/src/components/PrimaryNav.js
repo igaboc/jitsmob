d224a1e22e6ca376e2652cccb17f3e01fa833c94
igaboc <isabellegaboc@hotmail.com>
2018-01-19 14:21:27 +1100

    Fix spelling on exercises

--- web/src/components/PrimaryNav.js
+++ web/src/components/PrimaryNav.js
@@ -6,38 +6,38 @@ import './PrimaryNav.css';
 
 function PrimaryNav({
   menuClassWidth,
-        onMenuClick,
+  onMenuClick,
 
 }) {
-        return (
-                <Fragment>
+  return (
+    <Fragment>
 
-                        <div className="topnav row py-2" id="myTopnav">
-                                <div className="col-2 text-center success">
-                                        <img className="logo" src={jitsmobLogo} alt="JitsMob Logo" />
-                                </div>
-                                <div className="col-10 col-lg-9 pr-5">
-                                        <a className="ml-4 pt-3 pb-2" href="/excercises">Excercises</a>
-                                        <a className="mx-4 pt-3 pb-2" href="/admin">Admin (for now only)</a>
-                                        <a className="mx-4 pt-3 pb-2" href="/signin">Login (for now only)</a>
-                                        <span className="py-2 icon" onClick={(event) => { onMenuClick() }}>&#9776;</span>
-                                </div>
-                                <div className="col-lg-1" />
-                        </div>
+      <div className="topnav row py-2" id="myTopnav">
+        <div className="col-2 text-center success">
+          <img className="logo" src={jitsmobLogo} alt="JitsMob Logo" />
+        </div>
+        <div className="col-10 col-lg-9 pr-5">
+          <a className="ml-4 pt-3 pb-2" href="/exercises">Exercises</a>
+          <a className="mx-4 pt-3 pb-2" href="/admin">Admin (for now only)</a>
+          <a className="mx-4 pt-3 pb-2" href="/signin">Login (for now only)</a>
+          <span className="py-2 icon" onClick={(event) => { onMenuClick() }}>&#9776;</span>
+        </div>
+        <div className="col-lg-1" />
+      </div>
 
 
-                        <div className={`overlay ${menuClassWidth}`}>
-                                <a href="no-script-url: 0" className="closebtn" onClick={(event) => { onMenuClick() }} >&times;</a>
-                                <div className="overlay-content">
-                                        <a href="/admin">Admin (for now only)</a>
-                                        <a href="/signin">Login (for now only)</a>
-                                        <a href="/exercises">Exercises</a>
-                                        <a href="/">Home</a>
-                                        <a href="contact">Contact</a>
-                                </div>
-                        </div>
-                </Fragment>
-        );
+      <div className={`overlay ${menuClassWidth}`}>
+        <a href="no-script-url: 0" className="closebtn" onClick={(event) => { onMenuClick() }} >&times;</a>
+        <div className="overlay-content">
+          <a href="/admin">Admin (for now only)</a>
+          <a href="/signin">Login (for now only)</a>
+          <a href="/exercises">Exercises</a>
+          <a href="/">Home</a>
+          <a href="contact">Contact</a>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
 
 
