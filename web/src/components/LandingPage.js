458caab2df9f85cbe0d1fc28f1132fb7c619762f
Jared Hutchinson <jared.hutchinson99@gmail.com>
2018-01-20 10:44:10 +1100

    CSS alignment improvements

--- web/src/components/LandingPage.js
+++ web/src/components/LandingPage.js
@@ -9,30 +9,34 @@ function LandingPage({
     <div className='fonts'>
       <div className='landing-section-wrapper0'>
         <div className='landing-page-text'>
-          <h3 className='landing-page-para'>Boost Your</h3>
-          <h2 className='landing-page-para'>Jiu-Jitsu Performance</h2>
-          <p className='landing-page-para'>Thousands of Jiu-Jitsu athletes worldwide incorporate JitsMob into their daily workout routine</p>
-          <FindOutMoreButton />
-          <br />
+          <div className='responsive-group'>
+            <div className='responsive-row-main'>
+              <h3 className='landing-page-para'>Boost Your</h3>
+              <h2 className='landing-page-para'>Jiu-Jitsu Performance</h2>
+              <p className='landing-page-para'>Thousands of Jiu-Jitsu athletes worldwide incorporate JitsMob into their daily workout routine</p>
+              <FindOutMoreButton />
+              <br />
+            </div>
+          </div>
         </div>
       </div>
 
       <div className='landing-section-wrapper1'>
         <div className='landing-page-text'>
-          <div className='flex-group'>
-            <div className='flexbox'>
+          <div className='responsive-group'>
+            <div className='responsive-row-description'>
               <img src="http://placeholder.pics/svg/100x100" alt="Placeholder" />
-              <h3 className='landing-page-para text-flex'>Boost Performance</h3>
+              <h3 className='landing-page-para'>Boost Performance</h3>
               <p>Increased range of motion can generate power and promote efficiency of functional movements through improved position and posture.</p>
             </div>
-            <div className='flexbox'>
+            <div className='responsive-row-description'>
               <img src="http://placeholder.pics/svg/100x100" alt="Placeholder" />
-              <h3 className='landing-page-para text-flex'>Optimise Range of Motion</h3>
+              <h3 className='landing-page-para'>Optimise Range of Motion</h3>
               <p>Range of Motion is optimized through the strengthening of ligaments and tendons while simultaneously removing excess fascia through specific stretching routines.</p>
             </div>
-            <div className='flexbox'>
+            <div className='responsive-row-description'>
               <img src="http://placeholder.pics/svg/100x100" alt="Placeholder" />
-              <h3 className='landing-page-para text-flex'>Accelerate Recovery</h3>
+              <h3 className='landing-page-para'>Accelerate Recovery</h3>
               <p>Recovery is increased through the natural release of hyaluronic acid while simultaneously resetting the central nervous system.</p>
             </div>
           </div>
@@ -41,10 +45,16 @@ function LandingPage({
 
       <div className='landing-section-wrapper2'>
         <div className='landing-page-text2'>
-          <img src="http://placeholder.pics/svg/100x100" alt="Placeholder" />
-          <h3 className='landing-page-text2h landing-page-para'>HI, I’m Tao</h3>
-          <p className='landing-page-para'>Jitsmob aims to accelerate, improve and optimize your performance in Juijitsu.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <div className='responsive-group'>
+            <div className='responsive-row-about'>
+              <img src="http://placeholder.pics/svg/100x100" alt="Placeholder" />
+            </div>
+            <div className='responsive-row-about'>
+              <h3 className='landing-page-text2h landing-page-para'>HI, I’m Tao</h3>
+              <p className='landing-page-para'>Jitsmob aims to accelerate, improve and optimize your performance in Juijitsu.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+          </div>
         </div>
       </div>
 
