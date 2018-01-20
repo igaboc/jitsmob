4d49473a877cdb2f32bdebdcb58652219e2c922d
igaboc <isabellegaboc@hotmail.com>
2018-01-20 16:25:47 +1100

    Fix formatting on navbar

--- web/src/components/PrimaryNav.js
+++ web/src/components/PrimaryNav.js
@@ -20,28 +20,22 @@ function PrimaryNav({
           </a>
         </div>
         <div className="col-10 col-lg-9 pr-5">
-          {/* <span
-className="btn-purple"
-onClick={ (event) => {
-onClickSubscribe()
-}}
->
-Subscribe
-</span> */}
-        <div>
+        {/* <div>
           <Subscribe
-            onClickSubscribe={(event) => onClickSubscribe()}
-            buttonType='btn-purple'
-            buttonDescription='Subscribe'
+          onClickSubscribe={(event) => onClickSubscribe()}
+          buttonType='btn-purple'
+          buttonDescription='Subscribe'
           />
-        </div>
-                {/* <a className="ml-4 pt-3 pb-2" href="/exercises">Exercises</a>
-                <a className="mx-4 pt-3 pb-2" href="/admin">Admin (for now only)</a>
-                <a className="mx-4 pt-3 pb-2" href="/signin">Login (for now only)</a> */}
-                <NavLink activeClassName="active-navlink" className="navlink mx-4 pt-3 pb-2" to="/admin">Admin (for now only)</NavLink>
-                <NavLink activeClassName="active-navlink" className="navlink mx-4 pt-3 pb-2" exact to="/">Home</NavLink>
-                <NavLink activeClassName="active-navlink" className="navlink mx-4 pt-3 pb-2" to="/exercises">Exercises</NavLink>
-                <span className="py-2 icon" onClick={(event) => { onMenuClick() }}>&#9776;</span>
+        </div> */}
+          {/* <a className="ml-4 pt-3 pb-2" href="/exercises">Exercises</a>
+          <a className="mx-4 pt-3 pb-2" href="/admin">Admin (for now only)</a>
+        <a className="mx-4 pt-3 pb-2" href="/signin">Login (for now only)</a> */}
+          
+          <span className="navlink mx-4 pt-3 pb-2" onClick={ (event) => {onClickSubscribe() }}><div className='btn-purple'>SUBSCRIBE</div></span>
+          <NavLink activeClassName="active-navlink" className="navlink mx-4 pt-3 pb-2" exact to="/">Contact</NavLink>
+          <NavLink activeClassName="active-navlink" className="navlink mx-4 pt-3 pb-2" to="/exercises">Workouts</NavLink>
+          <NavLink activeClassName="active-navlink" className="navlink mx-4 pt-3 pb-2" to="/#about">About</NavLink>
+          <span className="py-2 icon" onClick={(event) => { onMenuClick() }}>&#9776;</span>
         </div>
         <div className="col-lg-1" />
       </div>
@@ -50,11 +44,12 @@ Subscribe
       <div className={`overlay ${menuClassWidth}`}>
         <a href="no-script-url: 0" className="closebtn" onClick={(event) => { onMenuClick() }} >&times;</a>
         <div className="overlay-content">
-          <a href="/admin">Admin (for now only)</a>
-          <a href="/signin">Login (for now only)</a>
-          <a href="/exercises">Exercises</a>
           <a href="/">Home</a>
-          <a href="contact">Contact</a>
+          <a href="/#about">About JitsMob</a>
+          <a href="/exercises">Workouts</a>
+          <a href="mailto:jitsmob@jitsmob.com">Contact Us</a>
+          <span className='subscribe' onClick={ (event) => {onClickSubscribe() }}>Subscribe</span>
+
         </div>
       </div>
     </Fragment>
