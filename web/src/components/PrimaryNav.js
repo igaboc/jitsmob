12a6b1981ddb82a89eb8cb26d9b758ff49d03175
igaboc <isabellegaboc@hotmail.com>
2018-01-20 14:56:24 +1100

    Add footer

--- web/src/components/PrimaryNav.js
+++ web/src/components/PrimaryNav.js
@@ -7,58 +7,58 @@ import { NavLink } from 'react-router-dom'
 
 function PrimaryNav({
   menuClassWidth,
-        onMenuClick,
-        onClickSubscribe
+  onMenuClick,
+  onClickSubscribe
 }) {
-        return (
-                <Fragment>
+  return (
+    <Fragment>
 
-                        <div className="topnav row py-2" id="myTopnav">
-                                <div className="col-2 text-center success">
-                                        <a href="/">
-                                                <img className="logo" src={jitsmobLogo} alt="JitsMob Logo" />
-                                        </a>
-                                </div>
-                                <div className="col-10 col-lg-9 pr-5">
-                                        {/* <span
-            className="btn-purple"
-            onClick={ (event) => {
-              onClickSubscribe()
-            }}
-          >
-            Subscribe
-          </span> */}
-                                        <div>
-                                                <Subscribe
-                                                        onClickSubscribe={(event) => onClickSubscribe()}
-                                                        buttonType='btn-purple'
-                                                        buttonDescription='Subscribe'
-                                                />
-                                        </div>
-                                        {/* <a className="ml-4 pt-3 pb-2" href="/exercises">Exercises</a>
-                                        <a className="mx-4 pt-3 pb-2" href="/admin">Admin (for now only)</a>
-                                        <a className="mx-4 pt-3 pb-2" href="/signin">Login (for now only)</a> */}
-                                        <NavLink activeClassName="active-navlink" className="navlink mx-4 pt-3 pb-2" to="/admin">Admin (for now only)</NavLink>
-                                        <NavLink activeClassName="active-navlink" className="navlink mx-4 pt-3 pb-2" exact to="/">Home</NavLink>
-                                        <NavLink activeClassName="active-navlink" className="navlink mx-4 pt-3 pb-2" to="/exercises">Exercises</NavLink>
-                                        <span className="py-2 icon" onClick={(event) => { onMenuClick() }}>&#9776;</span>
-                                </div>
-                                <div className="col-lg-1" />
-                        </div>
+      <div className="topnav row py-2" id="myTopnav">
+        <div className="col-2 text-center success">
+          <a href="/">
+            <img className="logo" src={jitsmobLogo} alt="JitsMob Logo" />
+          </a>
+        </div>
+        <div className="col-10 col-lg-9 pr-5">
+          {/* <span
+className="btn-purple"
+onClick={ (event) => {
+onClickSubscribe()
+}}
+>
+Subscribe
+</span> */}
+        <div>
+          <Subscribe
+            onClickSubscribe={(event) => onClickSubscribe()}
+            buttonType='btn-purple'
+            buttonDescription='Subscribe'
+          />
+        </div>
+                {/* <a className="ml-4 pt-3 pb-2" href="/exercises">Exercises</a>
+                <a className="mx-4 pt-3 pb-2" href="/admin">Admin (for now only)</a>
+                <a className="mx-4 pt-3 pb-2" href="/signin">Login (for now only)</a> */}
+                <NavLink activeClassName="active-navlink" className="navlink mx-4 pt-3 pb-2" to="/admin">Admin (for now only)</NavLink>
+                <NavLink activeClassName="active-navlink" className="navlink mx-4 pt-3 pb-2" exact to="/">Home</NavLink>
+                <NavLink activeClassName="active-navlink" className="navlink mx-4 pt-3 pb-2" to="/exercises">Exercises</NavLink>
+                <span className="py-2 icon" onClick={(event) => { onMenuClick() }}>&#9776;</span>
+        </div>
+        <div className="col-lg-1" />
+      </div>
 
 
-                        <div className={`overlay ${menuClassWidth}`}>
-                                <a href="no-script-url: 0" className="closebtn" onClick={(event) => { onMenuClick() }} >&times;</a>
-                                <div className="overlay-content">
-                                        <a href="/admin">Admin (for now only)</a>
-                                        <a href="/signin">Login (for now only)</a>
-                                        <a href="/exercises">Exercises</a>
-                                        <a href="/">Home</a>
-                                        <a href="contact">Contact</a>
-                                </div>
-                        </div>
-                </Fragment>
-        );
+      <div className={`overlay ${menuClassWidth}`}>
+        <a href="no-script-url: 0" className="closebtn" onClick={(event) => { onMenuClick() }} >&times;</a>
+        <div className="overlay-content">
+          <a href="/admin">Admin (for now only)</a>
+          <a href="/signin">Login (for now only)</a>
+          <a href="/exercises">Exercises</a>
+          <a href="/">Home</a>
+          <a href="contact">Contact</a>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
 
 
